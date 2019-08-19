71fd79ee30eeefdf3f5f1aa93da908a1aab27f5a
Andrew Widdowson <Andrew.Widdowson@iress.com>
2019-08-20 08:17:58 +1000

    Include dataflow

---
+++ AsyncDataManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace async_demo
+{ 
+    public class AsyncDataManager<TSource>// where TSource: new()
+    {
+        private BufferBlock<TSource> _queue;
+        private DataflowLinkOptions _linkOptions;
+        private List<ActionBlock<TSource>> _consumerGroup;
+
+        private void SetupProducerConsumer(int capacity)
+        {
+            _queue = new BufferBlock<TSource>(new DataflowBlockOptions { BoundedCapacity = capacity });
+            _linkOptions = new DataflowLinkOptions(){ PropagateCompletion = true };
+            _consumerGroup = new List<ActionBlock<TSource>>();
+        }
+
+        private void InitialiseConsumer(IEnumerable<ActionBlock<TSource>> consumerList)
+        {
+            foreach (var consumer in consumerList)
+            {
+                _queue.LinkTo(consumer, _linkOptions);
+                _consumerGroup.Add(consumer);
+            }
+        }
+
+        public async Task Produce(TSource value, CancellationToken token)
+        {
+            await _queue.SendAsync(value, token);
+        }
+
+        public async Task Produce(TSource value)
+        {
+            await _queue.SendAsync(value);
+        }
+
+        public void Completed()
+        {
+            _queue.Complete();
+            Task.WhenAll(_consumerGroup.Select(c => c.Completion)).Wait();
+        }
+
+        public void Completed(CancellationToken token)
+        {
+            _queue.Complete();
+            Task.WhenAll(_consumerGroup.Select(c => c.Completion)).Wait(token);
+        }
+
+        public AsyncDataManager(int boundedCapacity, IEnumerable<ActionBlock<TSource>> consumerList)
+        {
+            SetupProducerConsumer(boundedCapacity);
+            InitialiseConsumer(consumerList);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using System.Xml.Linq;
 
 namespace async_demo
@@ -10,89 +15,203 @@ namespace async_demo
     class Program
     {
         private static Random _random = new Random(100);
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(20);
+        private static string _fileName = "c:\\temp\\accounts.txt";
+        private static ReaderWriterLockSlim _fileLock = new ReaderWriterLockSlim();
+        private static AsyncDataManager<string> _asyncDataManager;
+
+
 
         static void Main(string[] args)
         {
-            var accountList = new List<string> {
-                "ACC1",
-                "ACC2",
-                "ACC3",
-                "ACC4",
-                "ACC5",
-                "ACC6",
-                "ACC7",
-                "ACC8",
-                "ACC9",
-                "ACC10",
-                "ACC11",
-                "ACC12",
-                "ACC13",
-                "ACC14",
-                "ACC15",
-                "ACC16",
-                "ACC17",
-                "ACC18",
-                "ACC19",
-                "ACC20"
-            };
-
-            List<Task<XElement>> tasks = new List<Task<XElement>>();
-
+            var cancellationTokenSourceForAsync = new CancellationTokenSource(/*10000*/);
             var sw = Stopwatch.StartNew();
-            foreach (var accountCode in accountList)
-            {
-                tasks.Add(GetDataAsync(accountCode));
-            }
-            foreach (var task in tasks)
-            {
-                try
-                {
-                    Console.WriteLine(task.Result);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"OMG Error! {ex.Message}");
-                }
+            //DownloadDataAsyncTasks(GetAccountsToProcess(100), cancellationTokenSourceForAsync.Token);
+            //Console.WriteLine($"Async tasks complete in {sw.Elapsed}");
 
-            }
-
-            Console.WriteLine($"Finished async in {sw.ElapsedMilliseconds} ms");
             sw.Restart();
-            foreach (var accountCode in accountList)
-            {
-                try
-                {
-                    Console.WriteLine(GetData(accountCode));
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"OMG Error! {ex.Message}");
-                }
-            }
-            Console.WriteLine($"Finished synchronous in {sw.ElapsedMilliseconds} ms");
+
+            var cancellationTokenSourceForDataflow = new CancellationTokenSource(/*10000*/);
+            DownloadDataWithDataFlow(GetAccountsToProcess(100), cancellationTokenSourceForDataflow.Token);
+            Console.WriteLine($"Dataflow tasks complete in {sw.Elapsed}");
+            //var cancellationTokenSourceForDataflow = new CancellationTokenSource(/*10000*/);
+            //DownloadDataWithDataFlow(GetAccountsToProcess(100), cancellationTokenSourceForDataflow.Token);
+            //Console.WriteLine($"Dataflow tasks complete in {sw.Elapsed}");
+
         }
 
-        private static Task<XElement> GetDataAsync(string accountCode)
+        private static void DownloadDataWithDataFlow(IEnumerable<string> accountList, CancellationToken token)
         {
-            return Task.Run(() => 
+            int concurrentTasks = 50;
+            var consumerList = new List<ActionBlock<string>>();
+            Enumerable.Range(0, concurrentTasks).ToList().ForEach(arg =>
+                consumerList.Add(new ActionBlock<string>(GetAndWriteData, new ExecutionDataflowBlockOptions { BoundedCapacity = 10 })));
+
+            _asyncDataManager = new AsyncDataManager<string>(50, consumerList);
+
+            try
             {
-                var waitTime = _random.Next(100, 1000);
-                Console.WriteLine($"Account {accountCode} waiting for {waitTime}");
-                Thread.Sleep(waitTime);
-                Console.WriteLine($"Account {accountCode} completed in {waitTime}");
-                if (accountCode == "ACC16")
+                foreach (var account in accountList)
+                    _asyncDataManager.Produce(account, token).Wait(token);
+
+                _asyncDataManager.Completed(token);
+            }
+            catch (AggregateException e)
+            {
+                e.Handle(ex =>
                 {
-                    try
-                    {                    
-                        return XElement.Parse($"<Account><AccountCode>{accountCode}</AccountCode></Account1>");
-                    }
-                    catch (Exception e)
+                    if (ex is TaskCanceledException)
                     {
-                        throw new Exception($"Error parsing xml for account {accountCode}.  The Error is {e.Message}",e);
+                        Console.WriteLine("This task has been cancelled.  Cleaning up temp files.");
+                        CleanUpTempFiles();
                     }
+
+                    if (ex is AccountFailedToDownloadException)
+                    {
+                        Console.WriteLine($"Account has failed to download. The error is {ex.Message}");
+                    }
+
+
+                    return ex is TaskCanceledException || ex is AccountFailedToDownloadException;
+                });
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine("This task has been cancelled.  Cleaning up temp files.");
+                CleanUpTempFiles();
+            }
+            catch (AccountFailedToDownloadException e)
+            {
+                Console.WriteLine($"Account has failed to download. The error is {e.Message}");
+            }
+        }
+
+        private static void DownloadDataAsyncTasks(IEnumerable<string> accountList, CancellationToken token)
+        {
+            var tasks = accountList.Select(x => GetDataAsync(x, token)).ToList();
+
+            Console.WriteLine($"TaskCount {tasks.Count}");
+
+            while (tasks.Any())
+            {
+                // ReSharper disable once CoVariantArrayConversion
+                var i = Task.WaitAny(tasks.ToArray());
+                try
+                {
+                    WriteToFile(tasks[i].Result);
                 }
-                return XElement.Parse($"<Account><AccountCode>{accountCode}</AccountCode></Account>");
-            });
+                catch (AggregateException e)
+                {
+                    e.Handle(ex =>
+                    {
+                        if (ex is TaskCanceledException)
+                        {
+                            Console.WriteLine("This task has been cancelled.  Cleaning up temp files.");
+                            CleanUpTempFiles();
+                        }
+
+                        if (ex is AccountFailedToDownloadException)
+                        {
+                            Console.WriteLine($"Account has failed to download. The error is {ex.Message}");
+                        }
+
+
+                        return ex is TaskCanceledException || ex is AccountFailedToDownloadException;
+                    });
+                }
+
+                tasks.RemoveAt(i);
+            }
+
+            Console.WriteLine($"Ended tasks running:{tasks.Count(x => !x.IsCompleted)}");
+        }
+
+        /// <summary>
+        /// Runs tasks and returns the results as they complete
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tasks"></param>
+        /// <returns></returns>
+        public static Task<Task<T>> [] Interleaved<T>(IEnumerable<Task<T>> tasks) 
+        { 
+            var inputTasks = tasks.ToList();
+
+            var buckets = new TaskCompletionSource<Task<T>>[inputTasks.Count]; 
+            var results = new Task<Task<T>>[buckets.Length]; 
+            for (int i = 0; i < buckets.Length; i++)  
+            { 
+                buckets[i] = new TaskCompletionSource<Task<T>>(); 
+                results[i] = buckets[i].Task; 
+            }
+
+            int nextTaskIndex = -1; 
+            Action<Task<T>> continuation = completed => 
+            { 
+                var bucket = buckets[Interlocked.Increment(ref nextTaskIndex)]; 
+                bucket.TrySetResult(completed); 
+            };
+
+            foreach (var inputTask in inputTasks) 
+                inputTask.ContinueWith(continuation, CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+
+            return results; 
+        }
+
+        private static async Task<XElement> GetDataAsync(string accountCode, CancellationToken token)
+        {
+            await _semaphore.WaitAsync(token);
+            try
+            {
+                var getDataTask = Task.Run(() => GetData(accountCode), token);
+                var result = await getDataTask;
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private static void GetAndWriteData(string accountCode)
+        {
+            // test to see if exceptions are handled
+            if (accountCode.EndsWith("5"))
+                throw new AccountFailedToDownloadException($"Error processing account {accountCode}");
+
+            var data = GetData(accountCode);
+            
+            WriteToFile(data);
+        }
+
+        private static void WriteToFile(XElement result)
+        {
+            _fileLock.EnterWriteLock();
+            try
+            {
+                File.AppendAllText(_fileName, result.ToString());
+            }
+            finally
+            {
+                _fileLock.ExitWriteLock();
+            }
+        }
+
+        private static void CleanUpTempFiles()
+        {
+            _fileLock.EnterWriteLock();
+            try
+            {
+                File.Delete(_fileName);
+            }
+            finally
+            {
+                _fileLock.ExitWriteLock();
+            }
 
         }
 
@@ -100,21 +219,45 @@ namespace async_demo
         {
             var waitTime = _random.Next(100, 1000);
             Console.WriteLine($"Account {accountCode} waiting for {waitTime}");
-            Thread.Sleep(waitTime);
+            Task.Delay(waitTime);
             Console.WriteLine($"Account {accountCode} completed in {waitTime}");
-            if (accountCode == "ACC16")
-            {
-                try
-                {                    
-                    return XElement.Parse($"<Account><AccountCode>{accountCode}</AccountCode></Account1>");
-                }
-                catch (Exception e)
-                {
-                    throw new Exception($"Error parsing xml for account {accountCode}.  The Error is {e.Message}",e);
-                }
-            }
+            // test to see if exceptions are handled
+            if (accountCode.EndsWith("5"))
+                throw new AccountFailedToDownloadException($"Error processing account {accountCode}");
+            //if (accountCode == "ACC16")
+            //{
+            //    try
+            //    {                    
+            //        return XElement.Parse($"<Account><AccountCode>{accountCode}</AccountCode></Account1>");
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        throw new Exception($"Error parsing xml for account {accountCode}.  The Error is {e.Message}",e);
+            //    }
+            //}
             return XElement.Parse($"<Account><AccountCode>{accountCode}</AccountCode></Account>");
 
         }
+
+        private static IList<string> GetAccountsToProcess(int count)
+        {
+            return Enumerable.Range(1, count).Select(i => $"ACC{i}").ToList();
+        }
+
+    }
+
+    
+    public class AccountFailedToDownloadException : Exception
+    {
+        public AccountFailedToDownloadException() : base()
+        {
+
+        }
+
+        public AccountFailedToDownloadException(string message) : base(message)
+        {
+
+        }
+
     }
 }
